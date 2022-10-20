72c9530ac6698ec9e248cf9dbbcd7e474e2dc207
julianboyne <julianboyne11@gmail.com>
2022-10-20 13:05:40 -0400

    julians thing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,6 +9,7 @@ const Landing = () => {
       <Link to="/the-well-styled-things">David's Things</Link><br />
       <Link to="/the-silly-things">Hunter's Things</Link><br />
       <Link to="/the-best-things">Mike's Things</Link><br />
+      <Link to="/the-julians-things">Julians's Things</Link><br />
     </>
   )
 }
